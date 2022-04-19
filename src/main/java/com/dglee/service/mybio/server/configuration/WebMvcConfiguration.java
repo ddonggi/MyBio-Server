faec56befcf366b84914fbac2ff29a754f4fc603
dglee <ldg6153@gmail.com>
2022-04-19 23:12:39 +0900

    ADD WebMvcConfiguration

---
+++ src/main/java/com/dglee/service/mybio/server/configuration/WebMvcConfiguration.java
@@ -0,0 +1,23 @@
+package com.dglee.service.mybio.server.configuration;
+/*
+ * Created by 이동기 on 2022-03-28
+ */
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class WebMvcConfiguration implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*");
+//                .allowedOrigins("https://apple.o2o.kr")
+//                .allowedHeaders("Access-Control-Allow-Origin","*")
+//                .allowedMethods("GET", "POST", "PUT", "DELETE");
+    }
+}
